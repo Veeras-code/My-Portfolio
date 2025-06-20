d3152d08219e3da4e05570893eef191f91cac109
Veeras-code <nagasai692004@gmail.com>
2025-06-20 16:31:35 +0530

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -28,7 +28,7 @@ function Projects() {
               imgPath={chatify}
               isBlog={false}
               title="Chatify"
-              description="funA real-time chatroom built with React.js, Firebase, and Material-UI. Features include messaging, image sharing, and emoji reactions."
+              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
               ghLink="https://github.com/Veeras-code/Chatify"
               demoLink="https://chatify-49.web.app/"
             />
@@ -39,7 +39,7 @@ function Projects() {
               imgPath={leaf}
               isBlog={false}
               title="Plant AI"
-              description="A deep learning model using PyTorch and ResNet34 that identifies plant leaf diseases with 98% accuracy using Kaggle’s dataset."
+              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
               ghLink="https://github.com/Veeras-code/Plant-AI-Master"
               demoLink="https://plant49-ai.herokuapp.com/"
             />
@@ -50,7 +50,7 @@ function Projects() {
               imgPath={emotion}
               isBlog={false}
               title="Face Recognition and Emotion Detection"
-              description="Facial emotion recognition using CNN on the FER-2013 dataset with Keras & TensorFlow. Face detection done using OpenCV."
+              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
               ghLink="https://github.com/Veeras-code/Face_Emotion_Detector/tree/main/Face_And_Emotion_Detection-master"
               demoLink="https://emotion-detector-demo.vercel.app/"
             />
